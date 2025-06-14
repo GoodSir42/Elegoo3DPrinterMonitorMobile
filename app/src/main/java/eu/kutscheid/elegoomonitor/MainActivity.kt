bb2e230c4e3ba52c48fc5ec8be987416910dc7f0
Matthias Kutscheid <matthias@kutscheid.eu>
2025-06-14 16:08:35 +0200

    remove greeting

--- app/src/main/java/eu/kutscheid/elegoomonitor/MainActivity.kt
+++ app/src/main/java/eu/kutscheid/elegoomonitor/MainActivity.kt
@@ -10,15 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import eu.kutscheid.elegoomonitor.data.UdpDataSource
 import eu.kutscheid.elegoomonitor.ui.theme.ElegooMonitorTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val dataSource = UdpDataSource()
@@ -30,9 +26,6 @@ class MainActivity : ComponentActivity() {
                 val dataItem by dataSource.startBroadcast().collectAsStateWithLifecycle(null)
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
-                        Greeting(
-                            name = "Android",
-                        )
                         Button(onClick = {
                         }) {
                             Text("Start!")
@@ -47,21 +40,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ElegooMonitorTheme {
-        Greeting("Android")
     }
 }
